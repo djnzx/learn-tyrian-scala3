ff8d5234e53564f62958f4219e15a56e33cb3951
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2026-05-10 13:17:42 +0300

    image added

--- tyrian/src/main/scala/djnz/Header.scala
+++ tyrian/src/main/scala/djnz/Header.scala
@@ -1,13 +1,23 @@
 package djnz
 
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
 import tyrian.*
 import tyrian.Html.cls
 import tyrian.Html.div
+import tyrian.Html.img
+import tyrian.Html.src
 
 object Header {
 
+  @js.native
+  @JSImport("url:../../static/img/fiery-lava-128x128.png", JSImport.Namespace)
+  val imageUrl: String = js.native
+
   def view: Html[Nothing] = div(cls := "p-1")(
-    div(cls := "space-y-4")("header1"),
+    img(
+      src := imageUrl
+    )
   )
 
 }
